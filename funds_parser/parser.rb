eb3bf04ce51d125e45e037a0fa3bf48a3c92f2f8
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-03-18 13:48:16 +0600

    Add fund categories parser

--- funds_parser/parser.rb
+++ funds_parser/parser.rb
@@ -1,39 +1,59 @@
 require 'csv'
 require 'json'
 
-csv = CSV.read('./funds.csv')
-coins = JSON.parse(File.read('../coins.json'))
 
-funds = []
-coins["coins"].each { |coin| coin["funds"] = [] }
+def import(csv, coins, funds, category)
+	puts "=================================== #{category} ======================================"
+	csv.shift
+	notMatched = []
 
-csv.shift
-notMatched = []
+	csv.each { |fund|
+		fundId = fund[0].downcase.gsub(/ /, '_').strip
+		funds << { id: fundId, name: fund[0], url: fund[1], category: category }
+		matched = false
 
-csv.each { |fund|
-	fundId = fund[0].downcase.gsub(/ /, '_').strip
-	funds << {id: fundId, name: fund[0], url: fund[1], description: ""}
-	matched = false
+		fund[2].split("\n")
+			.filter { |c| !c.strip.empty? }
+			.each { |c|
+				captures = c.strip.match(/[^()]+\((.+)\)/).captures
+				coinCode = captures[0].strip
 
-	fund[2].split("\n")
-		.filter { |c| !c.strip.empty? }
-		.each { |c|
-			captures = c.strip.match(/[^()]+\((.+)\)/).captures
-			coinCode = captures[0].strip
+				coins["coins"].each { |coin|
+					if coin["code"] == coinCode
+						matched = true
+						coin["funds"] << fundId
+					end
+				}
 
-			coins["coins"].each { |coin|
-				if coin["code"] == coinCode
-					matched = true
-					coin["funds"] << fundId
-				end
+				notMatched << coinCode unless matched
 			}
+	}
 
-			notMatched << coinCode unless matched
-		}
+	puts notMatched.uniq
+
+end
+
+
+coins = JSON.parse(File.read('../coins.json'))
+coins["coins"].each { |coin| coin["funds"] = [] }
+funds = []
+categories = []
+
+Dir["./funds/*"].each { |file|
+	name = file[10..-5]
+	name = name.gsub(/:/, '/')
+
+	id = name.downcase.gsub(/[^A-z0-9]/, '_').strip
+
+	order = file[8..8].to_i
+	categories << { id: id, name: name, order: order }
+
+	import(CSV.read(file), coins, funds, id)
 }
 
 coins["funds"] = funds
+coins["fund_categories"] = categories
 
-puts notMatched.uniq
+
 File.write('../coins.json', JSON.pretty_generate(coins, indent: '    '))
 
